f44a5fae744c6b015ba3a9b7a47926c689a84263
Sir-Veillance <bmckay900@gmail.com>
2021-05-04 11:58:45 -0400

    Adds full functionality to the create new database button in the mainwindow.
    A new database is created with full schema now. It is also added to a separate transaction_databases database that will track all databases used by the program to allow for easy reference in the load database menu.

--- Transaction-Analyzer/MainWindow.xaml.cs
+++ Transaction-Analyzer/MainWindow.xaml.cs
@@ -45,12 +45,81 @@ namespace Transaction_Analyzer
             databaseName = NewDBWindow.DatabaseName;
 
             // TODO: Hardcoded login info, could obviously be replaced with a better system
-            MySqlConnection conn = new("Server=localhost;Database=" + databaseName + ";port=3306;User Id=bmckay;password=!DatabasesUser2");
+            // MySqlConnection conn = new("Server=localhost;Database=" + databaseName + ";port=3306;User Id=bmckay;password=!DatabasesUser2");
+            MySqlConnection conn = new("Server=localhost;port=3306;uid=bmckay;password=!DatabasesUser2");
+            conn.Open();
+            string query = "SHOW DATABASES LIKE '" + databaseName + "';";
+            MySqlCommand existsCommand = new(query, conn);
 
-            AnalyzerMenu menu = new(conn);
-            this.Opacity = 0.5;
-            menu.ShowDialog();
-            this.Opacity = 1.0;
+            // datareader will return a boolean value of whether it found an existing database with the provided name
+            MySqlDataReader dataReader = existsCommand.ExecuteReader();
+            if (dataReader.Read())
+            {
+                MessageBox.Show("A database with this name already exists on your MySQL server. If it is not already available in the load menu then the database is being used for some other purpose",
+                                                                 "Confirmation",
+                                                                 MessageBoxButton.OK,
+                                                                 MessageBoxImage.Exclamation);
+            } else
+            {
+                dataReader.Close();
+                query = "INSERT INTO transaction_databases.dbnames VALUES ('" + databaseName + "');";
+                MySqlCommand databasesInsert = new(query, conn);
+                databasesInsert.ExecuteNonQuery();
+
+                // creating fresh database with full schema
+                query = "CREATE DATABASE " + databaseName + ";";
+                MySqlCommand createCommand = new(query, conn);
+                createCommand.ExecuteNonQuery();
+
+                query = "USE " + databaseName + ";";
+                MySqlCommand setDatabaseCommand = new(query, conn);
+                setDatabaseCommand.ExecuteNonQuery();
+
+                query = "CREATE TABLE Stock_Transaction (" +
+                        "TransactionID BIGINT PRIMARY KEY," +
+                        "Symbol VARCHAR(5)," +
+                        "TransactDate DATE," +
+                        "Quantity FLOAT," +
+                        "Price FLOAT," +
+                        "Type CHAR(1)" +
+                        ");";
+                MySqlCommand tableCreationCommand = new(query, conn);
+                tableCreationCommand.ExecuteNonQuery();
+
+                query = "CREATE TABLE Option_Transaction (" +
+                        "TransactionID BIGINT PRIMARY KEY," +
+                        "Symbol VARCHAR(5)," +
+                        "TransactDate DATE," +
+                        "Quantity FLOAT," +
+                        "Price FLOAT," +
+                        "Type CHAR(1)," +
+                        "Strike FLOAT," +
+                        "Expire DATE" +
+                        ");";
+                tableCreationCommand = new(query, conn);
+                tableCreationCommand.ExecuteNonQuery();
+
+                query = "CREATE TABLE Option_Removal (" +
+                        "TransactionID BIGINT PRIMARY KEY," +
+                        "Symbol VARCHAR(5)," +
+                        "Quantity FLOAT," +
+                        "RemovalDate DATE" +
+                        ");";
+                tableCreationCommand = new(query, conn);
+                tableCreationCommand.ExecuteNonQuery();
+
+                query = "CREATE TABLE Company (" +
+                        "Symbol VARCHAR(5) PRIMARY KEY," +
+                        "Name VARCHAR(5)" +
+                        ");";
+                tableCreationCommand = new(query, conn);
+                tableCreationCommand.ExecuteNonQuery();
+
+                AnalyzerMenu menu = new(conn);
+                this.Opacity = 0.5;
+                menu.ShowDialog();
+                this.Opacity = 1.0;
+            }
 
             // string query = "SELECT * FROM pc";
             // MySqlCommand cmd = new(query, conn);
